e8d696c6310ed55d2243de26a72101301d83396c
eirikb <devnull@localhost>
2010-09-27 21:00:19 +0200

    BootStrap with roles, admin user and requestmaps

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,29 @@
-class BootStrap {
+import p1.*
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+class BootStrap {
+	
+	def springSecurityService
+	
+	
+	def init = { servletContext -> createAuth() }
+	def destroy = {
+	}
+	
+	def void createAuth() {
+		def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+		
+		def adminUser = User.findByUsername('admin') ?: new User(
+				username: 'admin',
+				password: springSecurityService.encodePassword('admin'),
+				enabled: true).save(failOnError: true)
+		
+		
+		if (!adminUser.authorities.contains(adminRole)) {
+			UserRole.create adminUser, adminRole
+		}
+		
+		new Requestmap(url: '/account/*', configAttribute: 'ROLE_USER').save(failOnError: true)
+		new Requestmap(url: '/user', configAttribute: 'ROLE_ADMIN').save(failOnError: true)
+	}
 }
