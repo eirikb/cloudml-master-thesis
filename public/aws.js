dd7047e740a947df2409d300d069cf2b5f2bc61c
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-04 23:24:04 +0200

    Prototype inheritance again, just for Profile

--- public/aws.js
+++ public/aws.js
@@ -1,8 +1,20 @@
 var profile = function() {
+    var self = this;
+    this.Common = function(name, type) {
+        this.meta = {
+            name: name,
+            type: type,
+            required: []
+        };
+
+        this.toString = function() {
+            return name + ' (' + type + ')';
+        };
+    };
     this.AWS = {
         EC2: {
-            Instance: function(name) {
-                this.name = name;
+            Instance: function() {
+                self.Common.apply(this, arguments);
                 this.AvailabilityZone = '';
                 this.DisableApiTermination = '';
                 this.ImageId = '';
@@ -18,26 +30,27 @@ var profile = function() {
                 this.UserData = '';
                 this.Volumes = ['AWS::EC2::MountPoin'];
                 this.Description = '';
-                this.meta = {
-                    required: ['PlacementGroupName']
-                };
+                this.meta.required.push('PlacementGroupName');
             },
-            SecurityGroup: function(name) {
-                this.name = name;
+            SecurityGroup: function() {
+                self.Common.apply(this, arguments)
                 this.SecurityGroupIngress = 'AWS::EC"::SecurityGroupRole?';
                 this.GroupDescription = '';
                 this.Description = '';
-                this.meta = {
-                    required: ['GroupDescription']
-                };
+                this.meta.requied.push('GroupDescription');
             },
-            EIP : function(name) {
-                this.name = name;
+            EIP : function() {
+                self.Common.apply(this, arguments);
                 this.Description = '';
-                this.meta = {
-                    required: []
-                };
             }
         }
-    }
+    };
+    this.AWS.EC2.Instance.prototype = new this.Common();
+    this.AWS.EC2.SecurityGroup.prototype = new this.Common();
+    this.AWS.EC2.EIP.prototype = new this.Common();
+    this.Fn = {
+        FindInMap: function() {
+
+        }
+    };
 };
