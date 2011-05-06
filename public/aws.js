571613dddf7c38fba4f1218cd7ff29d5d708bdcb
Eirik <eirikb@eeeirik.localdomain>
2011-05-06 22:40:56 +0200

    Started workign on a node.js-solution for easier testing ... maybe, at least I hope

--- public/aws.js
+++ public/aws.js
@@ -1,45 +1,51 @@
 var Profile = function(Common) {
-    this.AWS = {
-        EC2: {
-            Instance: function() {
-                Common.apply(this, arguments);
-                this.AvailabilityZone = '';
-                this.DisableApiTermination = '';
-                this.ImageId = '';
-                this.KernelId = '';
-                this.KeyName = '';
-                this.Monitoring = '';
-                this.PlacementGroupName = '';
-                this.PrivateIpAddress = '';
-                this.RamDiskId = '';
-                this.SecurityGroups = ['AWS::EC2::SecurityGroup'];
-                this.SubnetId = '';
-                this.Tags = ['AWS::EC2::Tags'];
-                this.UserData = '';
-                this.Volumes = ['AWS::EC2::MountPoin'];
-                this.Description = '';
-                this.meta.required.push('PlacementGroupName');
-                this.init();
-            },
-            SecurityGroup: function() {
-                Common.apply(this, arguments)
-                this.SecurityGroupIngress = 'AWS::EC"::SecurityGroupRole?';
-                this.GroupDescription = '';
-                this.Description = '';
-                this.meta.required.push('GroupDescription');
-            },
-            EIP : function() {
-                Common.apply(this, arguments);
-                this.Description = '';
-            }
-        }
-    };
-    this.AWS.EC2.Instance.prototype = new Common();
-    this.AWS.EC2.SecurityGroup.prototype = new Common();
-    this.AWS.EC2.EIP.prototype = new Common();
-    this.Fn = {
-        FindInMap: function() {
+	var Common = typeof require !== 'undefined' ? require('common').Common: Common;
+	this.AWS = {
+		EC2: {
+			Instance: function() {
+				Common.apply(this, arguments);
+				this.AvailabilityZone = '';
+				this.DisableApiTermination = '';
+				this.ImageId = '';
+				this.KernelId = '';
+				this.KeyName = '';
+				this.Monitoring = '';
+				this.PlacementGroupName = '';
+				this.PrivateIpAddress = '';
+				this.RamDiskId = '';
+				this.SecurityGroups = ['AWS::EC2::SecurityGroup'];
+				this.SubnetId = '';
+				this.Tags = ['AWS::EC2::Tags'];
+				this.UserData = '';
+				this.Volumes = ['AWS::EC2::MountPoin'];
+				this.Description = '';
+				this.meta.required.push('PlacementGroupName');
+				this.init();
+			},
+			SecurityGroup: function() {
+				Common.apply(this, arguments)
+				this.SecurityGroupIngress = 'AWS::EC"::SecurityGroupRole?';
+				this.GroupDescription = '';
+				this.Description = '';
+				this.meta.required.push('GroupDescription');
+			},
+			EIP: function() {
+				Common.apply(this, arguments);
+				this.Description = '';
+			}
+		}
+	};
+	this.AWS.EC2.Instance.prototype = new Common();
+	this.AWS.EC2.SecurityGroup.prototype = new Common();
+	this.AWS.EC2.EIP.prototype = new Common();
+	this.Fn = {
+		FindInMap: function() {
 
-        }
-    };
+		}
+	};
 };
+
+if (typeof module !== 'undefined' && module.exports) {
+	exports.Profile = Profile;
+}
+
