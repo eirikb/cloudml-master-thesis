83bbefecd1bc1c991adeea7af578ba78e7db1aa1
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-09 00:05:36 +0200

    New setup

--- public/template.js
+++ public/template.js
@@ -1,121 +1,51 @@
-var Template = function(template, profile) {
+var _ = require('underscore');
+
+var Template = function(profile, template) {
 	var self = this,
 	k, i, l;
-	self.Resources = {};
 	self.meta = {
 		warnings: []
 	};
 
 	function merge(obj, data) {
-		for (var prop in data) {
-			if (data.hasOwnProperty(prop)) {
-				if (data[prop]) {
-					if (!obj.hasOwnProperty(prop)) {
-						self.meta.warnings.push('Profile does not have property ' + prop + ' for object ' + obj.toString());
-					}
-					obj[prop] = data[prop];
-				}
+		_.keys(data, function(key) {
+			if (!obj[key]) {
+				self.meta.warnings.push('Profile does not have property ' + prop + ' for object ' + obj.toString());
 			}
-		}
+			obj[key] = data[key];
+		});
 		return obj;
 	}
-
-	function cleanup(obj) {
-		for (var k in obj) {
-			if (obj[k].unused) {
-				delete obj[k];
-			}
-		}
-		return obj;
-	};
 
 	function create(name, data) {
-		var a = data.Type.split('::'),
-		objConstructor = profile[a[0]],
-		obj,
-		k,
-		i;
-		for (i = 1; i < a.length; i++) {
-			objConstructor = objConstructor[a[i]];
-		}
-		obj = merge(new objConstructor(name, data.Type), data.Properties);
-		cleanup(obj);
-		for (i = 0; i < obj.meta.required.length; i++) {
-			if (!obj[obj.meta.required[i]]) {
+		var constructor = profile,
+		obj;
+		_.each(data.Type.split('::'), function(ns) {
+			constructor = constructor[ns];
+		});
+		obj = new constructor();
+		obj.meta && _.each(obj.meta.required, function(required) {
+			if (!obj[required]) {
 				self.meta.warnings.push('Requied property ' + obj.meta.required[i] + ' is not set in ' + obj.toString());
 			}
-		}
+		});
 		return obj;
 	}
 
-	function findRef(ref) {
-		var k;
-		for (k in self.Parameters) {
-			if (k === ref) {
-				return self.Parameters[k]
-			}
-		}
-		for (k in self.Resources) {
-			if (k === ref) {
-				return self.Resources[k];
-			}
-		}
-		return null;
-	}
+	_.extend(self, template);
+    _.each(self.Resources, function(resource, key) {
+        self.Resources[key] = create(key, resource);
+    });
 
-	function appendMeta(col) {
-		for (var k in col) {
-			col[k].meta = col[k].meta ? col[k].meta: {};
-		}
-	}
-
-	function removeAllMeta(obj) {
-		if (typeof obj === 'object') {
-			delete obj['meta'];
-			for (var p in obj) {
-				removeAllMeta(obj[p]);
-			}
-		}
-	}
-
-	self.AWSTemplateFormatVersion = template.AWSTemplateFormatVersion;
-	self.Description = template.Description;
-	self.Parameters = template.Parameters;
-	self.Mappings = template.Mappings;
-    self.Outputs = template.Outputs;
-	appendMeta(self.Parameters);
-	appendMeta(self.Mappings);
-
-	for (k in template.Resources) {
-		self.Resources[k] = create(k, template.Resources[k]);
-	}
-	for (k in self.Resources) {
-		for (l in self.Resources[k]) {
-			if (self.Resources[k][l].constructor === Object) {
-				var ref = self.Resources[k][l]['Ref'];
-				if (ref) {
-					self.Resources[k][l] = new Ref(findRef(ref));
-				}
-			} else if (self.Resources[k][l].constructor === Array) {
-				for (i = 0; i < self.Resources[k][l].length; i++) {
-					var ref = self.Resources[k][l][i]['Ref'];
-					if (ref) {
-						self.Resources[k][l][i] = new Ref(findRef(ref));
-					}
-				}
-			}
-		}
-	}
-
-	this.toTemplate = function() {
+	this.serialize = function() {
 		removeAllMeta(self.Parameters);
 		removeAllMeta(self.Mappings);
 		var t = {
 			AWSTemplateFormatVersion: self.AWSTemplateFormatVersion,
 			Descriptoin: self.Description,
 			Parameters: self.Parameters,
-			Mappings: self.Mappings
-            Outputs: self.Outputs
+			Mappings: self.Mappings,
+			Outputs: self.Outputs
 		};
 		return t;
 	};
@@ -128,6 +58,6 @@ var Ref = function(ref) {
 };
 
 if (typeof module !== 'undefined' && module.exports) {
-	exports.Template = Template;
+	module.exports.Template = Template;
 }
 
