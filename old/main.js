ed9b909bdb6900d729a6c583bdc4fe0da103c535
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-27 00:43:56 +0200

    Restructuring

--- old/main.js
+++ old/main.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    ['EC2WebSiteSample-1.0.0.template'].forEach(function(aws) {
+    $(['EC2WebSiteSample-1.0.0.template']).each(function() {
         $.get('aws/' + aws, function(data) {
             var template = new Template($.parseJSON(data), new Profile());
             console.log(template, $.parseJSON(data));
---
+++ utils/buildprofile.js
@@ -0,0 +1,41 @@
+var http = require('http'),
+fs = require('fs'),
+scraper = require('scraper'),
+        pref = 'http://docs.amazonwebservices.com/AWSCloudFormation/latest/UserGuide/',
+resources = {};
+
+console.log('Building profile from %s', pref);
+
+scraper(pref + '_toc.html', function(err , $) {
+    var i = 0,
+    total;
+    console.log('Got list of resources');
+    total = $('#aws-product-property-reference a').each(function() {
+        var href = $(this).attr('href');
+        console.log('Fetching %s', href);
+        scraper(pref + href, function(err, $) {
+            var props = [],
+            obj = {};
+
+            $('.informaltable thead th').each(function() {
+                props.push($(this).text());
+            });
+
+            $('.informaltable tbody tr').each(function() {
+                $(this).find('td').each(function(i) {
+                    obj[props[i]] = $(this).text().trim().join('\n', '-');
+                });
+            });
+
+            resources[$r.find('h1').text().replace(/ \w*/, '')] = obj;
+
+            i++;
+            console.log(Math.floor((i / total) * 100) + '%');
+            if (i === total) {
+                fs.writeFileSync('profile.json', JSON.stringify(resources));
+                console.log('DONE');
+            }
+        });
+    }).size();
+});
+
