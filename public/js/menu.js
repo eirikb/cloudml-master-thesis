122f0832e0cddcaf713ad088d5288436603933ba
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-31 01:48:11 +0200

    REORDERING delux + iconhack

--- public/js/menu.js
+++ public/js/menu.js
@@ -16,6 +16,10 @@ cloudml.menu = (function() {
                     };
                     parent.push(obj);
                     if (type === _(types).last()) {
+                        obj.data = {
+                            title: type,
+                            icon: 'http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/16x16/categories/applications-internet-2.png'
+                        };
                         obj.metadata = {
                             draggable: true,
                             properties: data[key]
@@ -42,8 +46,8 @@ cloudml.menu = (function() {
                 },
                 drop_finish: function(data) {
                     var $o = $(data.o),
-            e = data.e;
-                
+                    e = data.e;
+
                     cloudml.diagram.addClass(e.offsetX, e.offsetY, $o.text(), $o.data('properties'));
                 }
             },
