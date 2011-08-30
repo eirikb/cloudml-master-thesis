fc4d45162f29c858afd0798aa4f2f99817b35f8b
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-30 22:12:25 +0200

    using ::

--- public/menu.js
+++ public/menu.js
@@ -1,21 +1,48 @@
 cloudml.menu = (function() {
-    function buildTree(obj) {
-        var t = [],
-        children;
-        _(obj).chain().keys().each(function(key) {
-            if (typeof obj[key] === 'object') {
-                children = buildTree(obj[key]);
-                t.push({
-                    data: key,
-                    children: children
-                });
+    function buildTree(data) {
+        var root = [];
+        _(data).chain().keys().each(function(key) {
+            var types = key.split('::'),
+            obj = _(root).chain().select(function(o) {
+                return o.data === types[0];
+            }).first().value();
+
+            if (_(obj).isUndefined()) {
+                obj = {
+                    data: _(types).first()
+                };
+                root.push(obj);
             }
+
+            _(types.slice(1)).chain().each(function(type, i) {
+                var child;
+                if (!obj.children) {
+                    obj.children = [];
+                }
+                child = _(obj.children).chain().select(function(c) {
+                    return c.data === type;
+                }).first().value();
+                if (_(child).isUndefined()) {
+                    child = {
+                        data: type
+                    };
+                    if (type === _(types).last()) {
+                        child.metadata = {
+                            draggable: true
+                        };
+                    }
+                    obj.children.push(child);
+                }
+                obj = child;
+            });
+
         });
-        return t;
+        console.log(root)
+        return root;
+
     }
 
     var init = function(data) {
-        console.log(data)
         $('#application strong').hide();
 
         $('#menu').jstree({
@@ -25,6 +52,9 @@ cloudml.menu = (function() {
             dnd: {
                 drop_check: function(data) {
                     console.log(arguments);
+                    _(arguments[0]).each(function(a, i) {
+                        console.log(i, $(a).data('draggable'));
+                    });
                     return false;
                 }
             },
