f46473a8327829b10c416a1069d49d4f817fe91b
Eirik Brandtzæg <eirikb@eirikb.no>
2011-09-14 14:41:52 +0200

    Junk! Stuff that is probably going to be removed or not used - why not a branch? oh well...

---
+++ junk/transform.js
@@ -0,0 +1,26 @@
+var t = {
+    'HW::Instance': {
+        aws: {
+            'AWS::EC2::Instance': {
+                'imageId': 'ImageId',
+                'flavor': 'InstanceType',
+                'key': 'KeyName'
+            }
+        },
+        rs: {
+            'server': {
+                'imageId': {
+                    'imageId': {
+                        type: 'Number'
+                    }
+                },
+                'flavor': {
+                    'flavorId': {
+                        type: 'Number'
+                    }
+                }
+            }
+        }
+    }
+};
+
