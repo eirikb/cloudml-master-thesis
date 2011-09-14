4f79de0df0fb6f6b05adb55447649f98b4ad8ebc
eirikb <eirikb@eirikb.no>
2011-09-14 18:57:50 +0200

    Transformation like a boss

--- entitytransformator.js
+++ entitytransformator.js
@@ -1,23 +1,32 @@
 //
 //  Transform entites for resources according to provider
 //
-exports.entityTransformation = function(type, data) {
+exports.entityTransformation = function(type, resource) {
     switch (type) {
     case 'aws':
-        aws(data);
+        return aws(resource);
         break;
     case 'racksace':
-        rackspace(data);
+        return rackspace(resource);
         break;
     default:
         console.error('Unknown type', type);
         break;
     }
-    return data;
 };
 
-function aws(data) {
-    data.Resources.forEach(function(resource) {
-    });
+function aws(r) {
+    var resource = {},
+    properties = {},
+    p = r.Properties;
+
+    properties.ImageId = '' + p.imageId;
+    properties.InstanceType = p.flavor;
+
+    resource[p.name] = {
+        Type: 'AWS::EC2::Instance',
+        Properties: properties
+    }
+    return resource;
 }
 
