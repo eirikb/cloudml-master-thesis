d6e0510abdc66c63ba2858d2941776344b80edc3
Eirik Brandtzæg <eirikb@eirikb.no>
2011-09-14 18:04:09 +0200

    Structuring models and stuff

--- preprocessor.js
+++ preprocessor.js
@@ -1,37 +1,10 @@
-var fs = require('fs');
-
-if (process.argv.length >= 3) {
-    var templateFile = process.argv[2];
-    readFile(templateFile);
-} else {
-    console.log('Please provide template file as argument');
-}
-
-exports.preprocess = function(resources) {
+exports.preprocess = function(data) {
     var parameters = mapParameters(data);
     data.Resources.forEach(function(resource) {
         processParameters(resource.Properties, parameters);
     });
     console.log(require('util').inspect(data, true, null));
 };
-
-function readFile(templateFile) {
-    fs.readFile(templateFile, function(err, data) {
-        if (!err) {
-            parseData(data);
-        } else {
-            console.error(err.message);
-        }
-    });
-}
-
-function parseData(data) {
-    try {
-        processResources(JSON.parse(data));
-    } catch(err) {
-        console.error(err.message);
-    }
-}
 
 function mapParameters(data) {
     var parameters = {},
