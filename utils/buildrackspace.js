8f6aab8b69d3b50c01ed224561faecb77468f014
Eirik Brandtzæg <eirikb@eirikb.no>
2011-09-14 10:21:37 +0200

    Extracting data from rackspace...

---
+++ utils/buildrackspace.js
@@ -0,0 +1,46 @@
+var $ = require('jquery'),
+http = require('http'),
+fs = require('fs'),
+path = require('path'),
+get = function(p, callback) {
+    var data = '';
+    http.get({
+        host: 'docs.rackspace.com',
+        path: '/servers/api/v1.0/' + p
+    },
+    function(res) {
+        res.on('data', function(c) {
+            data += c;
+        });
+        res.on('end', function() {
+            callback(data);
+        });
+    });
+},
+save = function(p) {
+    get(p, function(r) {
+        var name = path.basename(p);
+        console.log('Saving', name);
+        fs.writeFileSync(name, r);
+    });
+},
+donelist = [];
+
+function rec(p) {
+    if (donelist.indexOf(p) < 0) {
+        donelist.push(p);
+        get(p, function(r) {
+            $(r).find('[href!=]').each(function() {
+                var href = $(this).attr('href');
+                if (href.match(/\.xsd$/)) {
+                    rec(href);
+                } else if (href.match(/\.json/)) {
+                    save(href);
+                }
+            });
+        });
+    }
+}
+
+rec('');
+
