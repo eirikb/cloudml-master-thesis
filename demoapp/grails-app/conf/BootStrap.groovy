10f124f72714d87a0cb54a545fc3ac5ad61a653b
eirikb <eirikb@eirikb.no>
2011-09-26 19:16:24 +0000

    Dropped maven for now, but this really works - the original setup!

--- demoapp/grails-app/conf/BootStrap.groovy
+++ demoapp/grails-app/conf/BootStrap.groovy
@@ -5,18 +5,15 @@ class BootStrap {
 	def springSecurityService
 	
 	
-	def init = { 
-        servletContext -> createAuth() 
-    }
-
+	def init = { servletContext -> createAuth() }
+	def destroy = {
+	}
+	
 	def void createAuth() {
-		def userRole = Role.findByAuthority('ROLE_USER') ?: 
-            new Role(authority: 'ROLE_USER').save(failOnError: true)
-		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: 
-            new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+		def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
 		
-		def adminUser = User.findByUsername('admin') ?: 
-            new User(
+		def adminUser = User.findByUsername('admin') ?: new User(
 				username: 'admin',
 				password: springSecurityService.encodePassword('admin'),
 				enabled: true).save(failOnError: true)
