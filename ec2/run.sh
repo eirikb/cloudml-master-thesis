459636d4dad9c3e12ce9831194c647f1ffc8420d
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-05 01:38:26 +0200

    Helperscripts

---
+++ ec2/run.sh
@@ -0,0 +1,3 @@
+./ec2-api-tools/bin/ec2-run-instances -k dev --region eu-west-1 ami-24506250
+./ec2-api-tools/bin/ec2-run-instances -k dev --region eu-west-1 ami-24506250
+./ec2-api-tools/bin/ec2-run-instances -k dev --region eu-west-1 ami-24506250
