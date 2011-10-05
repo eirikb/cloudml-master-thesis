f21aa6a500b10f3451b2c447751c216f6d45d382
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-05 02:28:58 +0200

    Removed stuff that is now in readme

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -1,5 +1,3 @@
-echo -e "\nRun like this: . ./setup.sh\nPut your keys in pem-folder ( https://aws-portal.amazon.com/gp/aws/developer/account/index.html )\n"
-
 wget http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip
 unzip ec2-api-tools.zip
 mv ec2-api-tools-* ec2-api-tools
