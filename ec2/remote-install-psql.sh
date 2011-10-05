2f1dc58df4f1d8b9ba66d591d9ba4de00d3033f0
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-05 10:42:45 +0200

    hBa, not g dumbass

--- ec2/remote-install-psql.sh
+++ ec2/remote-install-psql.sh
@@ -4,5 +4,5 @@ sudo /etc/init.d/postgresql initdb
 sudo /etc/init.d/postgresql stop
 # Note that good old >> did not work here
 echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/postgresql.conf
-echo "host all all  0.0.0.0/0 md5" | sudo tee -a /var/lib/pgsql/data/pg_hga.conf
+echo "host all all  0.0.0.0/0 md5" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
 sudo /etc/init.d/postgresql start
