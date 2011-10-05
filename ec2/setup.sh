58609229fc1ec7868237f1bc6d0492875bbbe490
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-05 08:53:34 +0200

    EC2_HOME fullpath

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -1,6 +1,6 @@
 wget http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip
 unzip ec2-api-tools.zip
 mv ec2-api-tools-* ec2-api-tools
-export EC2_HOME=ec2-api-tools
+export EC2_HOME=$PWD/ec2-api-tools
 export EC2_PRIVATE_KEY=`ls $PWD/pem/pk*`
 export EC2_CERT=`ls $PWD/pem/cert*`
