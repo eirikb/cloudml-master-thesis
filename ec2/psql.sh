933725b0dccd0ed625aeac20ab9b4a75ca045c51
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-05 02:18:10 +0200

    And starting the db

--- ec2/psql.sh
+++ ec2/psql.sh
@@ -3,3 +3,4 @@ ssh -ti pem/dev.pem ec2-user@$1 sudo yum -y install postgresql postgresql-server
 ssh -ti pem/dev.pem ec2-user@$1 sudo /etc/init.d/postgresql initdb
 ssh -ti pem/dev.pem ec2-user@$1 'echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/pg_hga.conf'
 ssh -ti pem/dev.pem ec2-user@$1 'echo "host all all  0.0.0.0/0 md5" | sudo tee -a /var/lib/pgsql/data/postgresql.conf'
+ssh -ti pem/dev.pem ec2-user@$1 sudo /etc/init.d/postgresql start
