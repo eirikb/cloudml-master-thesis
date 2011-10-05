d1365cabc29d16b9058abfba8cb9b110cedfbe4f
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-05 09:04:23 +0200

    And installing postgresql

---
+++ ec2/remote-install-psql.sh
@@ -0,0 +1,8 @@
+sudo yum -y update
+sudo yum -y install postgresql postgresql-server
+sudo /etc/init.d/postgresql initdb
+sudo /etc/init.d/postgresql stop
+# Note that good old >> did not work here
+echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/postgresql.conf
+echo "host all all  0.0.0.0/0 md5" | sudo tee -a /var/lib/pgsql/data/pg_hga.conf
+sudo /etc/init.d/postgresql start
