b300d264f4b3c95e345fecc975c9c7a789453ed8
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-11 17:14:40 +0200

    CLEANUP2000

--- ec2/deploy.sh
+++
@@ -1,2 +0,0 @@
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i pem/dev.pem ../demoapp/target/bank*.war ec2-user@$1:~/
-sh inject.sh remote-deploy.sh $1
--- web/js/main.js
+++
@@ -1,9 +0,0 @@
-var cloudml = {};
-
-$(function() {
-    $.getJSON('profile.json', function(data) {
-        cloudml.menu.init(data);
-        cloudml.diagram.init(data);
-    });
-});
-
